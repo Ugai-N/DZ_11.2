72a7af7fc3894b664db85e022bab522dabcd975e
Nadezhda Ugay <777ugay@gmail.com>
2023-05-31 18:12:13 +0300

    add new func and docstrings to str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def make_upper(line):
+    '''возвращает строку со всеми заглавными буквами'''
     return line.upper()
+
+
+def make_title(line):
+    '''возвращает строку, где первые буквы заглавные'''
+    return line.title()
